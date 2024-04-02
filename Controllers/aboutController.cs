cb1820a27e162d12558b94cb767736284c1f5ddb
st10360194 <126887800+st10360194@users.noreply.github.com>
2024-04-02 21:50:30 +0200

    Add project files.

---
+++ Controllers/aboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace kEmporium.Controllers
+{
+    public class aboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
